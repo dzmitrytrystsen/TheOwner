5ac48c75bdfb684957ddc96c75ac38551de6c6c8
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-18 18:51:34 +0300

    Level set up

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField] public float fadeTime = 2f;
+
+    private Image fadePanel;
+    private Color currentColor = Color.black;
+
+	void Start ()
+	{
+        fadePanel = GetComponent<Image>();
+	}
+	
+	void Update ()
+	{
+        DoFadeIn();
+    }
+
+    private void DoFadeIn()
+    {
+        if (Time.timeSinceLevelLoad < fadeTime)
+        {
+            float alphaChange = Time.deltaTime / fadeTime;
+
+            currentColor.a -= alphaChange;
+            fadePanel.color = currentColor;
+        }
+
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
